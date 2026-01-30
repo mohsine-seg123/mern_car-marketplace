36d8d7a59d3d0d35cd8c34bb20cceeff1cdfae00
MOHSINE SEGAOUI <sagauimohsin@gmail.com>
2026-01-30 23:21:27 +0100

    Update base URL for API requests

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}`,
+  baseURL: `https://mern-car-marketplace-two.vercel.app/`,
   headers: {
     "Content-Type": "application/json",
   },
