14c3c43e62f6cb5b34bf566969094e1ba42a6406
MOHSINE SEGAOUI <sagauimohsin@gmail.com>
2026-01-30 23:34:49 +0100

    Update axios.js

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `https://mern-car-marketplace-two.vercel.app/`,
+  baseURL: `${import.meta.env.VITE_API_URL}`,
   headers: {
     "Content-Type": "application/json",
   },
