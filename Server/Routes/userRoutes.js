bfd6a489ee5ee1d4192a789b1edab8249d19e752
MOHSINE SEGAOUI <sagauimohsin@gmail.com>
2026-02-02 21:49:16 +0100

    Update userRoutes.js

--- Server/Routes/userRoutes.js
+++ Server/Routes/userRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
+router.get('/me',authController.protect,authController.me);
 
 module.exports=router;
