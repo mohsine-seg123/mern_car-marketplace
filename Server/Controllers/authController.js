9f6c0c32990b781479bdd7b8693c1b3cb553b0b2
MOHSINE SEGAOUI <sagauimohsin@gmail.com>
2026-02-02 22:07:53 +0100

    Update authController.js

--- Server/Controllers/authController.js
+++ Server/Controllers/authController.js
@@ -15,6 +15,8 @@ const createSendToken = (user, statusCode, res) => {
   const cookieOptions={
        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN *24*60*60*1000),
        httpOnly:true,
+      secure: true,       
+      sameSite: "none",
   }
 
   res.cookie('jwt',token,cookieOptions);
