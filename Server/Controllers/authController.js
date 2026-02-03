37ac52e6b24ae5fdb572cad603e56a4116731148
MOHSINE SEGAOUI <sagauimohsin@gmail.com>
2026-02-03 11:38:53 +0100

    Update authController.js

--- Server/Controllers/authController.js
+++ Server/Controllers/authController.js
@@ -92,3 +92,12 @@ exports.me=catchAsync(async(req,res,next)=>{
     }
    });
 });
+
+
+exports.logout = catchAsync(async (req, res) => {
+     res.cookie('jwt','loggedout',{
+    expires:new Date(Date.now()+10*1000),
+    httpOnly:true,
+   });
+    res.status(200).json({ status: 'success' });
+});
