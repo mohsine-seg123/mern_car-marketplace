0aa411bd3be40b4e908c08ad8c441eacf3155e94
MOHSINE SEGAOUI <sagauimohsin@gmail.com>
2026-02-03 12:01:56 +0100

    Update Layaut.jsx

--- Client/src/Pages/Layaut.jsx
+++ Client/src/Pages/Layaut.jsx
@@ -3,11 +3,11 @@ import Navbare from "../Components/layaut/Navbare";
 
 
 
-export default function Layaut({connecter,username}) {
+export default function Layaut({connecter,username,setconnecter,setuser}) {
 
     return (
       <>
-         <Navbare connecter={connecter} username={username} />
+         <Navbare connecter={connecter} username={username} setConnecter={setconnecter} setuser={setuser} />
          <Outlet />
       </>
     );
