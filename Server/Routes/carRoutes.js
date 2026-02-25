fb8a3e9950b3e415a7ee779936155e9c1ab9994f
MOHSINE SEGAOUI <sagauimohsin@gmail.com>
2026-02-25 15:19:52 +0000

    fix case-sensitive controller import for vercel

--- Server/Routes/carRoutes.js
+++ Server/Routes/carRoutes.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const router = express.Router();
-const carController = require('../controllers/carController');
+const carController = require('../Controllers/carController');
 
 router.get('/stats', carController.getCarStats);
 
